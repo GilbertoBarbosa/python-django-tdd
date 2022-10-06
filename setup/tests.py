79486154f7d5b48e8a14c1c22b3bc550db30616e
= <=>
2022-10-06 16:31:58 -0300

    Capítulos 1 e 2

--- setup/tests.py
+++ setup/tests.py
@@ -2,5 +2,20 @@ from django.test import LiveServerTestCase
 from selenium import webdriver
 
 class AnimaisTestCase(LiveServerTestCase):
+    
     def setUp(self):
-        pass
+        self.browser = webdriver.Chrome('/home/python/python-django-tdd/chromedriver')
+
+    def tearDown(self):
+        self.browser.quit()
+
+    #def test_para_verificar_se_a_janela_do_browser_esta_ok(self):
+    #    self.browser.get('https://www.alura.com.br')
+
+    def test_abre_janela_do_chrome(self):
+        self.browser.get(self.live_server_url)
+
+    def test_deu_ruim(self):
+        """Teste de exemplo de erro"""
+        self.fail('Teste falhou - deu ruim mesmo')
+        
